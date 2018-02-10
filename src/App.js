5f3ec6c1c23a618e769bdc2df3e9d345beb5159e
DanHodges <dhdgs690@gmail.com>
2018-02-10 11:19:12 -0600

    add prettier

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      user: "bob",
+    };
+  }
+  componentDidMount() {}
   render() {
     return (
       <div className="App">
